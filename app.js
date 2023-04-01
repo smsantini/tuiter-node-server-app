14ec10b70070da3c005f62ca0619fe3688cabfa0
Sophia Mishurny-Santini <mishurny-santini.s@northeastern.edu>
2023-04-01 19:42:39 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors'
+import HelloController
+    from "./controllers/hello-controller.js";
+import UserController
+    from "./controllers/users/users-controller.js";
+import TuitsController
+    from "./controllers/tuits/tuits-controller.js";
+
+const app = express()
+app.use(cors());
+app.use(express.json());
+// app.get('/hello', (req, res) => {res.send('Life is good!')})
+// app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
+HelloController(app)
+UserController(app)
+TuitsController(app)
+app.listen(process.env.PORT || 4000)
