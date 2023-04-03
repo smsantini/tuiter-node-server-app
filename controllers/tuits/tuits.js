ee490010053f8f776de75d8f41b1d65f9f561b67
Sophia Mishurny-Santini <mishurny-santini.s@northeastern.edu>
2023-04-02 20:06:49 -0400

    change logos

--- controllers/tuits/tuits.js
+++ controllers/tuits/tuits.js
@@ -1,14 +1,14 @@
 export default [
     {
         "_id": "123", "topic": "Space", "username": "SpaceX",
-        "handle": "@spacex", "time": "2h", "image": "spacex.png",
+        "handle": "@spacex", "time": "2h", "image": "spaceX-logo.png",
         "title": "SpaceX's Mission",
         "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
         "liked": true, "likes": 2345, "replies": 123, "retuits": 432
     },
     {
         "_id": "234", "topic": "Traffic", "username": "The Boring Company",
-        "handle": "@boringcompany", "time": "2h", "image": "boring2.jpg",
+        "handle": "@boringcompany", "time": "2h", "image": "boring.png",
         "title": "The Boring Company fixing traffic",
         "tuit": "The Boring Company (TBC) is an American infrastructure and tunnel construction services company founded by Elon Musk. Its ongoing and proposed projects are designed for intra-city ('loop') transit systems.",
         "liked": true, "likes": 2345, "replies": 321, "retuits": 432
